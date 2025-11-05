9081960180f4a2f70ebde6a9378d638bfb09aace
Christian Aguilar <cbaguilar@g.ucla.edu>
2025-11-04 22:10:03 -0500

    Added more verbose logging

--- internal/systemservice/bluerock/systemstate.go
+++ internal/systemservice/bluerock/systemstate.go
@@ -145,6 +145,7 @@ func FromRawData(rawData []byte) (*BluerockState, error) {
 	var raw RawBluerockState
 	//unmarshal rawData into raw
 	err := json.Unmarshal(rawData, &raw)
+	logger.info("Raw data unmarshalled: %+v", raw)
 	if err != nil {
 		return nil, err
 	}
