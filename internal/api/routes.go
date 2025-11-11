ca7caae931c61f430b566aaa729b7c318ab1f488
Christian Aguilar <cbaguilar@g.ucla.edu>
2025-11-10 21:56:37 -0500

    Added route aliasing.

--- internal/api/routes.go
+++ internal/api/routes.go
@@ -20,6 +20,7 @@ func SetupRouter(ingestion *systemservice.DataIngestionService) *gin.Engine {
 	// This is the v0 route, which we will re-implement for backwards compatibility
 	// with the old Javascript server.
 	r.POST("/UploadDataNew", SaveSensorDataHandler(ingestion))
+	r.POST("/uploadSensorDataNew", SaveSensorDataHandler(ingestion)) // alias
 
 	return r
 }
