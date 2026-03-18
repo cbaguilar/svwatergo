e493a08c8b64dbd093bf0a28cddd0ec39a48f446
Christian Aguilar <cbaguilar@g.ucla.edu>
2026-03-17 20:03:56 -0400

    Wire render-plots flag in embedding source matrix

--- scripts/train_eval_actuation_embedding_source_matrix.sh
+++ scripts/train_eval_actuation_embedding_source_matrix.sh
@@ -27,6 +27,7 @@ AUX_PLC_TARGET_MODE="${AUX_PLC_TARGET_MODE:-raw}"
 AUX_PLC_FEATURE_COLS="${AUX_PLC_FEATURE_COLS:-plc_pca1,plc_pca2,plc_pca3,plc_pca4,plc_pca5,plc_pca6,plc_pca7,plc_pca8}"
 AUX_PLC_WEIGHT="${AUX_PLC_WEIGHT:-0.1}"
 PANN_PCA_WEIGHT="${PANN_PCA_WEIGHT:-0.0}"
+RENDER_PLOTS="${RENDER_PLOTS:-yes}"
 
 resolve_site_paths() {
   local site="$1"
@@ -148,6 +149,7 @@ for TRAIN_SPEC in "${DOMAIN_SPECS[@]}"; do
     --learning-rate "$LEARNING_RATE" \
     --weight-decay "$WEIGHT_DECAY" \
     --eval-every "$EVAL_EVERY" \
+    --render-plots "$RENDER_PLOTS" \
     --best-model-metric "$BEST_MODEL_METRIC" \
     --best-model-split "$BEST_MODEL_SPLIT" \
     --multilabel-pos-weight "$MULTILABEL_POS_WEIGHT" \
@@ -187,6 +189,7 @@ for TRAIN_SPEC in "${DOMAIN_SPECS[@]}"; do
       --learning-rate "$LEARNING_RATE" \
       --weight-decay "$WEIGHT_DECAY" \
       --eval-every 1 \
+      --render-plots "$RENDER_PLOTS" \
       --best-model-metric "$BEST_MODEL_METRIC" \
       --best-model-split "$BEST_MODEL_SPLIT" \
       --multilabel-pos-weight "$MULTILABEL_POS_WEIGHT" \
